b6b17b9d702e71f2a3e5018bd7ceaeaf2374f2ed
Prathap <prathapva@outlook.com>
2025-02-20 17:52:32 +0530

    added figlet

---
+++ backend/fruits/index.js
@@ -0,0 +1,7 @@
+const apple = require("./apple");
+const banana = require("./banana");
+const mango = require("./mango");
+
+let pandlu = [apple, banana, mango]
+
+module.exports = pandlu;
---
+++ backend/math.js
@@ -0,0 +1,18 @@
+const sum = (a,b)=> a+b;
+const minus = (a,b)=> a-b;
+const mul = (a,b)=> a*b;
+
+const g = 9.8;
+
+const pi = 3.14;
+
+let obj = {
+    sum : sum,
+    minus : minus,
+    mul : mul,
+    pi : pi,
+    g : g
+
+}
+
+module.exports = obj;
